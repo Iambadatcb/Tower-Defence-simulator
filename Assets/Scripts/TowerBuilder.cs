a895de042552cd166f17a5cac5cdc637b7e1a867
Iambadatcb <gviduks.cepuliuks@gmail.com>
2025-03-09 13:04:02 +0200

    Initial commit

---
+++ Assets/Scripts/TowerBuilder.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TowerBuilder : MonoBehaviour
+{
+    public LayerMask buildableGround;
+    public Transform cursor;
+    public GameObject towerPrefab;
+
+    
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out RaycastHit hit, buildableGround)){
+            cursor.position = hit.point;
+
+            if(Input.GetMouseButtonDown(0)){
+                Instantiate(towerPrefab, cursor.position, cursor.rotation);
+            }
+        }
+    }
+}
